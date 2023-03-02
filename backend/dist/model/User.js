682ab9193ab7f80a13d30e7f0b1289655519cc22
Tomas Storc <tomas.storc@gmail.com>
2023-03-02 09:35:38 +0100

    removed node modules

--- backend/dist/model/User.js
+++ backend/dist/model/User.js
@@ -6,7 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = __importDefault(require("mongoose"));
 const userSchema = new mongoose_1.default.Schema({
     username: {
-        min: [3, "password must be at least 3 characters long"],
+        min: [3, "username must be at least 3 characters long"],
         max: [15, "maximum number of characters exceeeded"],
         required: [true, "username is required"],
         type: String,
