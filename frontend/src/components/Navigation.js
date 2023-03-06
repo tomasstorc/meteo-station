e0e17f27f2de5fc76dd6b202077db31d52962029
Marketa Polakova <polakovamarketa@email.cz>
2023-03-06 12:47:01 +0100

    dashboard

---
+++ frontend/src/components/Navigation.js
@@ -0,0 +1,53 @@
+import React from "react";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import DevicesOtherIcon from "@mui/icons-material/DevicesOther";
+import LogoutIcon from "@mui/icons-material/Logout";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  Avatar,
+  Divider,
+  ListItemIcon,
+  ListItemText,
+  MenuItem,
+  MenuList,
+  Paper,
+  Link,
+} from "@mui/material";
+
+function Navigation() {
+  return (
+    <Paper sx={{ width: 250, maxWidth: "100%", height: "100vh" }}>
+      <div className="d-flex justify-content-center my-2">
+        {" "}
+        <Avatar src="/broken-image.jpg" />
+      </div>
+
+      <MenuList>
+        <MenuItem>
+          <ListItemIcon>
+            <DashboardIcon />
+          </ListItemIcon>
+          <Link href="/" underline="none" color="inherit">
+            {" "}
+            <ListItemText>Dashboard</ListItemText>
+          </Link>
+        </MenuItem>
+        <MenuItem>
+          <ListItemIcon>
+            <DevicesOtherIcon />
+          </ListItemIcon>
+          <ListItemText>Devices management</ListItemText>
+        </MenuItem>
+        <Divider />
+        <MenuItem>
+          <ListItemIcon>
+            <LogoutIcon />
+          </ListItemIcon>
+          <ListItemText>Logout</ListItemText>
+        </MenuItem>
+      </MenuList>
+    </Paper>
+  );
+}
+
+export default Navigation;
--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -2,6 +2,9 @@ import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { parseToken } from "../redux/loginSlice";
 import { Navigate } from "react-router-dom";
+import DashboardItem from "../components/DashboardItem";
+import { Container, Typography } from "@mui/material";
+import Navigation from "../components/Navigation";
 
 const Dashboard = () => {
   const dispatch = useDispatch();
@@ -11,9 +14,14 @@ const Dashboard = () => {
   }, [dispatch]);
 
   return (
-    <div>
-      Dashboard
-      {!user && <Navigate replace to={"/login"} />}
+    <div className="row">
+      <Navigation className="col" />
+      <Container className="col mt-3">
+        <Typography variant="h4"> Dashboard</Typography>
+
+        <DashboardItem name={"Thermometer - kitchen"} data="23°C" />
+        {!user && <Navigate replace to={"/login"} />}
+      </Container>
     </div>
   );
 };
