56f7e99c21374ab204046d4d566ed351e0c8f8b6
Tomas Storc <tomas.storc@gmail.com>
2023-05-28 08:07:53 +0200

    sort asc

--- backend/utils/covertToLocaleString.ts
+++ backend/utils/covertToLocaleString.ts
@@ -10,7 +10,7 @@ export default function convertToLocaleString(data: any) {
   });
   // sort data by date
   finalData.sort((a: any, b: any) => {
-    return new Date(b.date).getTime() - new Date(a.date).getTime();
+    return new Date(a.date).getTime() - new Date(b.date).getTime();
   });
   return finalData;
 }
